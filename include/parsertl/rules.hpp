fd2f7c2517fb83ffdf9bf3729d1f5f50855ff42d
BenHanson <jamin.hanson@gmail.com>
2023-05-15 21:09:24 +0100

    More use of auto

--- include/parsertl/rules.hpp
+++ include/parsertl/rules.hpp
@@ -525,8 +525,7 @@ namespace parsertl
 
         id_type token_id(const string& name_) const
         {
-            typename string_id_type_map::const_iterator iter_ =
-                _terminals.find(name_);
+            auto iter_ = _terminals.find(name_);
 
             if (iter_ == _terminals.end())
             {
@@ -613,8 +612,7 @@ namespace parsertl
             }
             else
             {
-                typename string_id_type_map::const_iterator iter_ =
-                    _non_terminals.find(_start);
+                auto iter_ = _non_terminals.find(_start);
 
                 if (iter_ != _non_terminals.end())
                     start_ = iter_->second;
