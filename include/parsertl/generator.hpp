a1c6c2cf86620635ab55af86976abb8dd757df54
BenHanson <jamin.hanson@gmail.com>
2023-11-19 19:27:41 +0000

    Moved reduce/reduce fix below the error msg construction

--- include/parsertl/generator.hpp
+++ include/parsertl/generator.hpp
@@ -854,15 +854,6 @@ namespace parsertl
                 }
                 else
                 {
-                    if (lhs_.action == action::reduce &&
-                        rhs_.action == action::reduce &&
-                        // Take the earlier rule on reduce/reduce error
-                        rhs_.param < lhs_.param)
-                    {
-                        lhs_.param = rhs_.param;
-                        modified_ = true;
-                    }
-
                     error_ = true;
                 }
             }
@@ -879,6 +870,15 @@ namespace parsertl
                     ss_);
                 ss_ << " conflict.\n";
                 warnings_ += ss_.str();
+
+                if (lhs_.action == action::reduce &&
+                    rhs_.action == action::reduce &&
+                    // Take the earlier rule on reduce/reduce error
+                    rhs_.param < lhs_.param)
+                {
+                    lhs_.param = rhs_.param;
+                    modified_ = true;
+                }
             }
 
             return modified_;
